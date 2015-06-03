150b5344157df919af973db0029b94156c598eb4
Tim Kramer <tim@thethinkery.net>
2015-06-02 21:11:11 -0700

    uninstall

---
+++ admin/sql/uninstall.knowledgebase.sql
@@ -0,0 +1,11 @@
+--
+-- Drop table `#__knowledgebase_categories`
+--
+
+DROP TABLE IF EXISTS `#__knowledgebase_categories`;
+
+--
+-- Drop table `#__knowledgebase_items`
+--
+
+DROP TABLE IF EXISTS `#__knowledgebase_items`;
